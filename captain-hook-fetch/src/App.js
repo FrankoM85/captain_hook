ea3b1403847be834d66c43984cfcacda1ff231c4
FrankoM85 <frankmoln85@gmail.com>
2021-02-16 10:15:09 +0100

    creating example of hook fetch

---
+++ captain-hook-fetch/src/App.js
@@ -0,0 +1,12 @@
+import Photos from './components/Photos';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Photos />
+    </div>
+  );
+}
+
+export default App;
